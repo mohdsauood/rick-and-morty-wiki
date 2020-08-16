2ce811ecb3531d7d209fa391c7fdd9f6b972f3ef
mohdsauood <35588119+mohdsauood@users.noreply.github.com>
2020-08-17 01:37:47 +0400

    fixed cl error

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
+import { motion } from 'framer-motion';
 import { homeTextVariant, homeImgVariant } from '../framerVariants';
 export default function HomeHeader() {
   return (
